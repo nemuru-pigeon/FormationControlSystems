d41f3631a644b6f881eb9d9a3d6c39dbc1c0c54a
nemuru-pigeon <z1592054@hotmail.com>
2024-12-18 04:07:44 +0100

    整理了前四节的代码

--- compute_error.m
+++ compute_error.m
@@ -1,12 +1,19 @@
 function error = compute_error(z_matrix, z_star, iteration)
-    error = zeros(iteration, 1);  
-    for iter = 1:iteration
-        temp_error = 0;  
-        for i = 1:7
-            diff_x = z_matrix(i, 1, iter) - z_star(i, 1);
-            diff_y = z_matrix(i, 2, iter) - z_star(i, 2);
-            temp_error = temp_error + sqrt(diff_x^2 + diff_y^2);  
-        end
-        error(iter) = temp_error;  
+% z_matrix: 
+% z_star: 
+% iteration: 
+
+% initialize error matrix recording errors for every iteration
+error = zeros(iteration, 1);
+
+for iter = 1:iteration
+    temp_error = 0;  
+    for i = 1:7
+        diff_x = z_matrix(i, 1, iter) - z_star(i, 1);
+        diff_y = z_matrix(i, 2, iter) - z_star(i, 2);
+        temp_error = temp_error + sqrt(diff_x^2 + diff_y^2);  
     end
+    error(iter) = temp_error;  
+end
+
 end
